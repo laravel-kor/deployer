08781efa0f4199e5d9462820f77eb05c548cd08c
moon0326 <moon.kyong@demandmedia.net>
2014-07-25 17:04:31 -0700

    adding shared dirs

--- deploy.php
+++ deploy.php
@@ -6,18 +6,10 @@ server('production', 'laravel.co.kr', '1013')
     ->user('deploy')
     ->path('/var/www/laravel.co.kr');
 
+set('shared_dirs',['app/storage']);
+set('shared_files',[]);
 set('repository', 'https://github.com/laravel-kor/laravel.co.kr.git');
-
-
-// Symfony shared dirs
-set('shared_dirs', []);
-
-// Symfony shared files
-set('shared_files', []);
-
-// Symfony writeable dirs
 set('writeable_dirs', ['app/storage']);
-
 
 task('chown', function() {
     run('sudo chown -R root:www-data current');
@@ -27,12 +19,21 @@ task('database:migrate', function() {
     run("php current/artisan migrate");
 });
 
+task('laravel:create_storage_dirs', function() {
+    run("mkdir current/app/storage/sessions");
+    run("mkdir current/app/storage/views");
+    run("mkdir current/app/storage/meta");
+    run("mkdir current/app/storage/logs");
+    run("mkdir current/app/storage/cache");
+});
+
 task('deploy', [
     'deploy:start',
     'deploy:prepare',
     'deploy:update_code',
     'deploy:shared',
     'deploy:writeable_dirs',
+    'laravel:create_storage_dirs',
     'deploy:vendors',
     'deploy:symlink',
     'database:migrate',
